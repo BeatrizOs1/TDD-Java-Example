8b0eab0cdddf0c15c0e949057437a8256f4d0afa
BeatrizOs1 <beatrizdeoliveira45@hotmail.com>
2023-08-30 23:17:08 -0300

    Update Relogio.java

--- src/main/java/com.relogio/Relogio.java
+++ src/main/java/com.relogio/Relogio.java
@@ -10,7 +10,6 @@ public class Relogio {
     private long fim; 
 
     public Relogio() {
-        // Inicializa os atributos com valores padrão
         horas = 0;
         minutos = 0;
         segundos = 0;
@@ -19,7 +18,6 @@ public class Relogio {
         fim = 0;
     }
 
-    // Método que recebe as horas, minutos e segundos e os atribui aos atributos correspondentes
     public void programar(int horas, int minutos, int segundos) {
         if (horas < 0 || horas > 23 || minutos < 0 || minutos > 59 || segundos < 0 || segundos > 59) {
             throw new IllegalArgumentException("Parâmetros inválidos para programar o relógio");
@@ -29,32 +27,27 @@ public class Relogio {
         this.segundos = segundos;
     }
 
-    // Método que zera os atributos de horas, minutos e segundos
     public void reiniciar() {
         horas = 0;
         minutos = 0;
         segundos = 0;
     }
 
-    // Método que marca o tempo atual como o início do intervalo e ativa o atributo cronometro
     public void iniciarCronometro() {
         inicio = System.currentTimeMillis();
         cronometro = true;
     }
 
-    // Método que marca o tempo atual como o fim do intervalo e desativa o atributo cronometro
     public void pararCronometro() {
         fim = System.currentTimeMillis();
         cronometro = false;
     }
 
-    // Método que retorna a diferença entre o fim e o início do intervalo em segundos
     public double tempoDecorrido() {
         double diferenca = (fim - inicio) / 1000.0;
         return diferenca;
     }
 
-    // Método que retorna uma string com a hora no formato desejado (24h ou AM/PM)
     public String imprimirHora(boolean ampm) {
         String hora = "";
         
@@ -76,7 +69,6 @@ public class Relogio {
         
         }
 
-        // Método que atualiza o horário quando recebe indicação de passagem do tempo do Sistema Operacional
      public void atualizar(int segundos) {
         this.segundos += segundos;
         if (this.segundos > 59) {
@@ -95,7 +87,6 @@ public class Relogio {
         }
     }
 
-    // Métodos getters para acessar os atributos privados da classe Relogio.
     public int getHoras() {
         return horas;
     }
